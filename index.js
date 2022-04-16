6d1a4f5920b2d3aafba4d2c1c6dd48783a2151d0
Lugano Abel <lugano.ngulwa@gmail.com>
2022-04-16 22:30:00 +0300

    Parse requests path

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const http = require('http');
+const url =  require('url');
 const port = 3000
-const server = http.createServer((req,res)=>
-{
+const server = http.createServer((req,res)=>{
+    
+    const parsedUrl = url.parse(req.url,true);
+    const path = parsedUrl.pathname
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
     res.end('Hello World!');
+
+    console.log('Request received on path',trimmedPath);
 });
 
 server.listen(port,()=>{
