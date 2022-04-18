37b5b4992da2a6740d61a289491ce8a250d8ac9f
Lugano Abel <lugano.ngulwa@gmail.com>
2022-04-18 18:15:44 +0300

    Parse HTTP methods

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const http = require('http');
 const url =  require('url');
 const port = 3000
 const server = http.createServer((req,res)=>{
-    
+
     const parsedUrl = url.parse(req.url,true);
     const path = parsedUrl.pathname
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method;
     res.end('Hello World!');
 
-    console.log('Request received on path',trimmedPath);
+    console.log('Request received on path '+ trimmedPath + ' with method '+ method );
 });
 
 server.listen(port,()=>{
