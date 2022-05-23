91f299557caf1a5faffef72be650e6af58e1f2d7
Lugano Abel <lugano.ngulwa@gmail.com>
2022-05-23 09:24:48 +0300

    Add service checks

--- index.js
+++ index.js
@@ -1,81 +1,29 @@
-const http = require('http');
-const https = require('https');
-const url =  require('url');
-const config = require('./lib/config');
-const fs = require('fs');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+/*
+ * Primary file for API
+ *
+ */
 
-helpers.sendTwilioSms('654051622','Hello',(err)=>{
-    console.log('this was the error',err);
+// Dependencies
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-});
+// Declare the app
+const app = {};
 
-const httpServer = http.createServer((req,res)=>{
-     unifiedServer(req,res);
-});
+// Init function
+app.init = ()=>{
 
-httpsServerOptions = {
-    "cert" : fs.readFileSync('./https/cert.pem'),
-    "key" : fs.readFileSync('./https/key.pem'),
-}
+  // Start the server
+  server.init();
 
-const httpsServer = https.createServer(httpsServerOptions,(req,res)=>{
-    unifiedServer(req,res);
-});
-
-const unifiedServer = (req,res) => {
-    const parsedUrl = url.parse(req.url,true);
-    const path = parsedUrl.pathname
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    const method = req.method;
-    const queryStringObject = parsedUrl.query;
-    const headers = req.headers;
-    const buffer = [];
-    req.on("data", (data) => {
-        buffer.push(data);
-    });
-    req.on("end", () => {
-        body = Buffer.concat(buffer).toString('utf8');
-       
-        const choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-        const data = {
-            'trimmedPath' : trimmedPath,
-            'queryStringObject' : queryStringObject,
-            'method' : method,
-            'payload': helpers.parseJsonToObject(body),
-            'headers': headers,
-        };
-     
-        choosenHandler(data,(statusCode,payLoad)=>{
-            
-            statusCode = typeof(statusCode) === 'number' ? statusCode : 200; 
-            payLoad = typeof(payLoad) == 'object' ? payLoad : {};
-            payLoad = JSON.stringify(payLoad);
-            
-            res.setHeader('Content-Type','application/json');
-            res.writeHead(statusCode);
-            res.end(payLoad);
-            console.log('Returning this response', statusCode,payLoad);
-        });
- });
-
+  // Start the workers
+  workers.init();
 
 };
 
-httpServer.listen(config.httpPort,()=>{
-    console.log(`Server listening at port ${config.httpPort}`);
-});
-
-httpsServer.listen(config.httpsPort,()=>{
-    console.log(`Server listening at port ${config.httpsPort}`);
-});
+// Self executing
+app.init();
 
 
-router = {
-    'ping' : handlers.ping,
-    'users' : handlers.users,
-    'tokens' : handlers.tokens,
-    'checks' : handlers.checks,
-};
+// Export the app
+module.exports = app;
