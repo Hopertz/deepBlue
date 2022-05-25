9e32ae05986f926ecaa3bbeacb15c9d8aabc8cc3
Lugano Abel <lugano.ngulwa@gmail.com>
2022-05-25 19:55:19 +0300

    Add signup

--- public/app.js
+++ public/app.js
@@ -20,7 +20,7 @@ app.client.request = (headers,path,method,queryStringObject,payload,callback)=>{
   // Set defaults
   headers = typeof(headers) == 'object' && headers !== null ? headers : {};
   path = typeof(path) == 'string' ? path : '/';
-  method = typeof(method) == 'string' && ['POST','GET','PUT','DELETE'].indexOf(method) > -1 ? method : 'GET';
+  method = typeof(method) == 'string' && ['POST','GET','PUT','DELETE'].indexOf(method.toUpperCase()) > -1 ? method.toUpperCase() : 'GET';
   queryStringObject = typeof(queryStringObject) == 'object' && queryStringObject !== null ? queryStringObject : {};
   payload = typeof(payload) == 'object' && payload !== null ? payload : {};
   callback = typeof(callback) == 'function' ? callback : false;
@@ -41,7 +41,7 @@ app.client.request = (headers,path,method,queryStringObject,payload,callback)=>{
   }
 
   // Form the http request as a JSON type
-  const xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open(method, requestUrl, true);
   xhr.setRequestHeader("Content-type", "application/json");
 
@@ -58,15 +58,15 @@ app.client.request = (headers,path,method,queryStringObject,payload,callback)=>{
   }
 
   // When the request comes back, handle the response
-  xhr.onreadystatechange = ()=>{
+  xhr.onreadystatechange = ()=> {
       if(xhr.readyState == XMLHttpRequest.DONE) {
-        const statusCode = xhr.status;
-        const responseReturned = xhr.responseText;
+        let statusCode = xhr.status;
+        let responseReturned = xhr.responseText;
 
         // Callback if requested
         if(callback){
           try{
-            const parsedResponse = JSON.parse(responseReturned);
+            let parsedResponse = JSON.parse(responseReturned);
             callback(statusCode,parsedResponse);
           } catch(e){
             callback(statusCode,false);
@@ -77,7 +77,73 @@ app.client.request = (headers,path,method,queryStringObject,payload,callback)=>{
   }
 
   // Send the payload as JSON
-  const payloadString = JSON.stringify(payload);
+  let payloadString = JSON.stringify(payload);
   xhr.send(payloadString);
 
+};
+
+// Bind the forms
+app.bindForms = ()=>{
+  document.querySelector("form").addEventListener("submit", (e)=>{
+
+    // Stop it from submitting
+    e.preventDefault();
+    let formId = this.id;
+    let path = this.action;
+    let method = this.method.toUpperCase();
+
+    // Hide the error message (if it's currently shown due to a previous error)
+    document.querySelector("#"+formId+" .formError").style.display = 'hidden';
+
+    // Turn the inputs into a payload
+    const payload = {};
+    let elements = this.elements;
+    for(let i = 0; i < elements.length; i++){
+      if(elements[i].type !== 'submit'){
+        let valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
+        payload[elements[i].name] = valueOfElement;
+      }
+    }
+
+    // Call the API
+    app.client.request(undefined,path,method,undefined,payload,function(statusCode,responsePayload){
+      // Display an error on the form if needed
+      if(statusCode !== 200){
+
+        // Try to get the error from the api, or set a default error message
+        let error = typeof(responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
+
+        // Set the formError field with the error text
+        document.querySelector("#"+formId+" .formError").innerHTML = error;
+
+        // Show (unhide) the form error field on the form
+        document.querySelector("#"+formId+" .formError").style.display = 'block';
+
+      } else {
+        // If successful, send to form response processor
+        app.formResponseProcessor(formId,payload,responsePayload);
+      }
+
+    });
+  });
+};
+
+// Form response processor
+app.formResponseProcessor = (formId,requestPayload,responsePayload)=>{
+  let functionToCall = false;
+  if(formId == 'accountCreate'){
+      console.log('Account created');
+    // @TODO Do something here now that the account has been created successfully
+  }
+};
+
+// Init (bootstrapping)
+app.init = ()=>{
+  // Bind all form submissions
+  app.bindForms();
+};
+
+// Call the init processes after the window loads
+window.onload = ()=>{
+  app.init();
 };
